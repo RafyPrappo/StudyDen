e501745160f16ce586c9e39bcfdc172b31f9e716
Raquib1011 <shammo.raquib@gmail.com>
2026-04-16 22:40:31 +0600

    Modified UI to support changing window size and mobile view

--- client/src/components/ui/Container.jsx
+++ client/src/components/ui/Container.jsx
@@ -1,6 +1,6 @@
 export default function Container({ children, className = "" }) {
   return (
-    <div className={`mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8 ${className}`}>
+    <div className={`mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8 overflow-x-hidden ${className}`}>
       {children}
     </div>
   );
